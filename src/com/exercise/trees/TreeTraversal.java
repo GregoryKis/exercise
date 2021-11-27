559ecf8e801ec817fbefb3401c716287b0af5ee3
Gregory Kiselman <gregory.kiselman@gmail.com>
2021-11-27 14:47:50 +0200

    solved questions

--- src/com/exercise/trees/TreeTraversal.java
+++ src/com/exercise/trees/TreeTraversal.java
@@ -7,25 +7,75 @@ import java.util.List;
 
 public class TreeTraversal {
 
-    public List<Integer> preorder(TreeNode root) {
+    public List<Integer> preOrder(TreeNode root) {
 
         if (root == null)
             return new ArrayList<>();
 
         ArrayList<Integer> ret = new ArrayList<>();
 
-        return recPreorder(ret, root);
+        return recPreOrder(ret, root);
     }
 
-    private ArrayList<Integer> recPreorder(ArrayList<Integer> res, TreeNode root) {
+    private ArrayList<Integer> recPreOrder(ArrayList<Integer> res, TreeNode root) {
 
         res.add(root.val);
 
         if (root.left != null)
-            recPreorder(res, root.left);
+            recPreOrder(res, root.left);
 
         if (root.right != null)
-            recPreorder(res, root.right);
+            recPreOrder(res, root.right);
+
+        return res;
+    }
+
+    public List<Integer> inOrder(TreeNode root) {
+
+        if (root == null)
+            return new ArrayList<>();
+
+        ArrayList<Integer> ret = new ArrayList<>();
+
+        return recInOrder(ret, root);
+    }
+
+    private ArrayList<Integer> recInOrder(ArrayList<Integer> res, TreeNode root) {
+
+        if (root.left != null) {
+            recInOrder(res, root.left);
+        }
+
+        res.add(root.val);
+
+        if (root.right != null)
+            recInOrder(res, root.right);
+
+        return res;
+    }
+
+
+    public List<Integer> postOrder(TreeNode root) {
+
+        if (root == null)
+            return new ArrayList<>();
+
+        ArrayList<Integer> ret = new ArrayList<>();
+
+        return recPostOrder(ret, root);
+    }
+
+    private ArrayList<Integer> recPostOrder(ArrayList<Integer> res, TreeNode root) {
+
+        if (root.left != null) {
+            recPostOrder(res, root.left);
+        }
+
+        if (root.right != null) {
+            recPostOrder(res, root.right);
+        }
+
+        res.add(root.val);
 
         return res;
     }
@@ -34,35 +84,60 @@ public class TreeTraversal {
         TreeTraversal traversal = new TreeTraversal();
 
         TreeNode t0 = null;
-        List<Integer> preorderT0 = traversal.preorder(t0);
-        assertTreeOrder(new ArrayList<>(), preorderT0, "t1");
+        List<Integer> preOrderT0 = traversal.preOrder(t0);
+        List<Integer> inOrderT0 = traversal.inOrder(t0);
+        List<Integer> postOrderT0 = traversal.postOrder(t0);
+        assertTreeOrder(new ArrayList<>(), preOrderT0, "PreOrder - t1");
+        assertTreeOrder(new ArrayList<>(), inOrderT0, "InOrder - t1");
+        assertTreeOrder(new ArrayList<>(), postOrderT0, "PostOrder - t1");
 
         TreeNode t1 = new TreeNode(1);
-        List<Integer> preorderT1 = traversal.preorder(t1);
-        assertTreeOrder(Collections.singletonList(1), preorderT1, "t1");
+        List<Integer> preOrderT1 = traversal.preOrder(t1);
+        List<Integer> inOrderT1 = traversal.inOrder(t1);
+        List<Integer> postOrderT1 = traversal.postOrder(t1);
+        assertTreeOrder(Collections.singletonList(1), preOrderT1, "PreOrder - t1");
+        assertTreeOrder(Collections.singletonList(1), inOrderT1, "InOrder - t1");
+        assertTreeOrder(Collections.singletonList(1), postOrderT1, "PostOrder - t1");
 
         TreeNode t2 = new TreeNode(1, new TreeNode(2), null);
-        List<Integer> preorderT2 = traversal.preorder(t2);
-        assertTreeOrder(Arrays.asList(1, 2), preorderT2, "t2");
+        List<Integer> preOrderT2 = traversal.preOrder(t2);
+        List<Integer> inOrderT2 = traversal.inOrder(t2);
+        List<Integer> postOrderT2 = traversal.postOrder(t2);
+        assertTreeOrder(Arrays.asList(1, 2), preOrderT2, "PreOrder - t2");
+        assertTreeOrder(Arrays.asList(2, 1), inOrderT2, "InOrder - t2");
+        assertTreeOrder(Arrays.asList(2, 1), postOrderT2, "PostOrder - t2");
 
         TreeNode t3 = new TreeNode(1, null, new TreeNode(2));
-        List<Integer> preorderT3 = traversal.preorder(t3);
-        assertTreeOrder(Arrays.asList(1, 2), preorderT3, "t3");
+        List<Integer> preOrderT3 = traversal.preOrder(t3);
+        List<Integer> inOrderT3 = traversal.inOrder(t3);
+        List<Integer> postOrderT3 = traversal.postOrder(t3);
+        assertTreeOrder(Arrays.asList(1, 2), preOrderT3, "PreOrder - t3");
+        assertTreeOrder(Arrays.asList(1, 2), inOrderT3, "InOrder - t3");
+        assertTreeOrder(Arrays.asList(2, 1), postOrderT3, "PostOrder - t3");
 
         TreeNode t4 = new TreeNode(1, new TreeNode(2, new TreeNode(3), null), null);
-        List<Integer> preorderT4 = traversal.preorder(t4);
-        List<Integer> resArrT4 = Arrays.asList(1, 2, 3);
-        assertTreeOrder(resArrT4, preorderT4, "t4");
+        List<Integer> preOrderT4 = traversal.preOrder(t4);
+        List<Integer> inOrderT4 = traversal.inOrder(t4);
+        List<Integer> postOrderT4 = traversal.postOrder(t4);
+        assertTreeOrder(Arrays.asList(1, 2, 3), preOrderT4, "PreOrder - t4");
+        assertTreeOrder(Arrays.asList(3, 2, 1), inOrderT4, "InOrder - t4");
+        assertTreeOrder(Arrays.asList(3, 2, 1), postOrderT4, "PostOrder - t4");
 
         TreeNode t5 = new TreeNode(1, null, new TreeNode(2, null, new TreeNode(3)));
-        List<Integer> preorderT5 = traversal.preorder(t5);
-        List<Integer> resArrT5 = Arrays.asList(1, 2, 3);
-        assertTreeOrder(resArrT5, preorderT5, "t5");
+        List<Integer> preOrderT5 = traversal.preOrder(t5);
+        List<Integer> inOrderT5 = traversal.inOrder(t5);
+        List<Integer> postOrderT5 = traversal.postOrder(t5);
+        assertTreeOrder(Arrays.asList(1, 2, 3), preOrderT5, "PreOrder - t5");
+        assertTreeOrder(Arrays.asList(1, 2, 3), inOrderT5, "InOrder - t5");
+        assertTreeOrder(Arrays.asList(3, 2, 1), postOrderT5, "PostOrder - t5");
 
-        TreeNode t6 = new TreeNode(3, new TreeNode(1), new TreeNode(2));
-        List<Integer> preorderT6 = traversal.preorder(t6);
-        List<Integer> resArrT6 = Arrays.asList(3, 1, 2);
-        assertTreeOrder(resArrT6, preorderT6, "t6");
+        TreeNode t6 = new TreeNode(1, new TreeNode(2), new TreeNode(3));
+        List<Integer> preOrderT6 = traversal.preOrder(t6);
+        List<Integer> inOrderT6 = traversal.inOrder(t6);
+        List<Integer> postOrderT6 = traversal.postOrder(t6);
+        assertTreeOrder(Arrays.asList(1, 2, 3), preOrderT6, "PreOrder - t6");
+        assertTreeOrder(Arrays.asList(2, 1, 3), inOrderT6, "InOrder - t6");
+        assertTreeOrder(Arrays.asList(2, 3, 1), postOrderT6, "PostOrder - t6");
 
     }
 
@@ -73,7 +148,8 @@ public class TreeTraversal {
         }
         for (int i = 0; i < expected.size(); i++) {
             if (expected.get(i) != actual.get(i)) {
-                System.out.println("FAIL " + testInfo + " - node difference. exp=" + expected.get(i) + ", actual=" + actual.get(i));
+                System.out.println("FAIL " + testInfo + " - node difference. exp=" + expected.get(i) + ", actual=" + actual.get(i) + ", listExpected=" + expected + ", listActual=" + actual);
+                break;
             }
         }
     }
