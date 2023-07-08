593a9e33bb1bef37f0dde5f06e21c1aaf7f68027
GregoryKis <gregory.kiselman@gmail.com>
2023-07-08 17:42:54 +0300

    String

--- src/com/exercise/strings/Subsequence.java
+++ src/com/exercise/strings/Subsequence.java
@@ -1,5 +1,8 @@
 package com.exercise.strings;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Subsequence {
     public boolean isSubsequence(String s, String t) {
         int i=0;
@@ -12,7 +15,44 @@ public class Subsequence {
         return j==t.length();
     }
 
+    public boolean canConstruct(String ransomNote, String magazine) {
+        int[] cArr=new int[26];
+        for(int i=0;i<magazine.length();i++){
+            cArr[magazine.charAt(i)-'a']++;
+        }
+        for(int i=0;i<ransomNote.length();i++){
+            cArr[ransomNote.charAt(i)-'a']--;
+            if(cArr[ransomNote.charAt(i)-'a']<0){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public boolean isIsomorphic(String s, String t) {
+
+        Map<Character, Character> mapS=new HashMap<>();
+        Map<Character, Character> mapT=new HashMap<>();
+        for(int i=0;i<s.length();i++){
+            if(mapS.containsKey(s.charAt(i)) || mapT.containsKey(t.charAt(i))){
+                System.out.println(s.charAt(i));
+                if(!mapS.containsKey(s.charAt(i))||!mapT.containsKey(t.charAt(i)) || mapS.get(s.charAt(i)) != t.charAt(i) || mapT.get(t.charAt(i))!=s.charAt(i))
+                    return false;
+                System.out.println("S "+mapS);
+                System.out.println("T "+mapT);
+            }else{
+                System.out.println("S "+mapS);
+                System.out.println("T "+mapT);
+                mapS.put(s.charAt(i), t.charAt(i));
+                mapT.put(t.charAt(i), s.charAt(i));
+            }
+        }
+        return true;
+    }
+
     public static void main(String[] args) {
-        System.out.println(new Subsequence().isSubsequence("abasdfcasd","abc"));
+//        System.out.println(new Subsequence().isSubsequence("abasdfcasd","abc"));
+//        System.out.println(new Subsequence().canConstruct("ab", "a"));
+        System.out.println(new Subsequence().isIsomorphic("badc","baba"));
     }
 }
